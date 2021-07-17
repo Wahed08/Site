dd10314561ac7fb4e614dfc5a962a1730b8f4243
Wahed08 <wahedfarhad123@gmail.com>
2021-07-17 15:51:35 +0600

    new one

---
+++ FullStack/client/src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  let url = "";
+  return (
+    <div className="header">
+      <div class="container">
+        <nav class="main-nav">
+          <a href={url}>
+            <h2>Website</h2>
+          </a>
+          <ul class="right-menu">
+            <li>
+              <a href={url}>About</a>
+            </li>
+            <li>
+              <a href={url}>Services</a>
+            </li>
+            <li>
+              <a href="http://wahed08.github.io">Portfolio</a>
+            </li>
+            <li>
+              <a href={url}>Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
