7bfa4c28911e271c3c947a6218134baaa5460986
Wahed08 <wahedfarhad123@gmail.com>
2021-08-22 12:08:21 +0600

    ..

---
+++ FullStack/client/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="main-footer">
+      <section class="footer">
+        <h3>Copyright &copy; 2021 - CRUD - App</h3>
+      </section>
+    </div>
+  );
+};
+
+export default Footer;
